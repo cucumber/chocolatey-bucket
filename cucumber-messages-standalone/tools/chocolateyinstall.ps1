5bab01ce1ec405e77c45abe5b2fd7b34972cd1e9
jreleaserbot <jreleaser@kordamp.org>
2026-04-06 00:09:00 +0000

    cucumber-messages-standalone v0.3.0

--- cucumber-messages-standalone/tools/chocolateyinstall.ps1
+++ cucumber-messages-standalone/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.23.0 at 2026-04-05T22:48:33.954541881Z
+# Generated with JReleaser 1.23.0 at 2026-04-06T00:08:46.772505111Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'cucumber-messages-standalone-0.2.14-windows-x86_64'
+$app_home = Join-Path $package 'cucumber-messages-standalone-0.3.0-windows-x86_64'
 $app_exe = Join-Path $app_home 'bin/cucumber-messages.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'cucumber-messages' `
-    -Url 'https://github.com/cucumber/messages-cli/releases/download/v0.2.14/cucumber-messages-standalone-0.2.14-windows-x86_64.zip' `
-    -Checksum '85289573397437b939cf83a54a9c36b636c60db651f7301d6b0af2e7caf00987' `
+    -Url 'https://github.com/cucumber/messages-cli/releases/download/v0.3.0/cucumber-messages-standalone-0.3.0-windows-x86_64.zip' `
+    -Checksum '2c5f125ae4402e9009c6e07e7f87aafd82682fbe8314aa597976bee45d3ed505' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
